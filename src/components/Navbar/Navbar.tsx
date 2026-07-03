436d9a109d673144300d2176418d139227ff6ac3
SenseiTumelo <tmotjopi@gmail.com>
2026-07-03 15:59:54 +0200

    nav bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import style from "./Navbar.module.css"
+import { Text } from '../Text/Text'
 
 export const Navbar = () => {
   return (
     <nav>
+        <img src="" alt="P" />
         <div>
+            <Text variant='h2'>Shop Online</Text>
+            <Text variant='h2'>Find in Store</Text>
+            <Text variant='h2'>Learn More</Text>
         </div>
+
     </nav>
   )
 }
