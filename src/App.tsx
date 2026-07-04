26aa114f9a7020a6b7ad65d3082b76df9429133f
senseiTumelo <tmotjopi@gmail.com>
2026-07-04 05:26:35 +0200

    navigation arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Navbar } from "./components/Navbar/Navbar"
 import { Hero } from "./components/Hero/Hero"
-
+import { Route, Routes } from "react-router-dom"
+//pages
+import { More } from "./pages/More"
+import { SearchStore } from "./pages/SearchStore"
+import { Shop } from "./pages/Shop"
+import { Home } from "./pages/Home"
 
 function App() {
 
   return (
-    <>
+    <> 
     <Navbar />
-    <Hero />
+    <Routes>
+      <Route path="/home" element={<Home />}/>
+      <Route path="/shop" element={<More />}/>
+      <Route path="/Search" element={<SearchStore />}/>
+      
+    </Routes>
       
     </>
   )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import style from "./Navbar.module.css"
+import { Link } from "react-router-dom";
 import { Text } from '../Text/Text'
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className={style["nav-container"]}>
+      <div className={style["nav-content"]}>
         <img src="" alt="P" />
-        <div>
-            <Text variant='h2'>Shop Online</Text>
-            <Text variant='h2'>Find in Store</Text>
-            <Text variant='h2'>Learn More</Text>
+        <div className={style['nav-links']}>
+          <Link to={"/home"} className={style['nav-link']}>Home</Link>
+          <Link to={"/shop"} className={style['nav-link']}>Shop</Link>
+          <Link to={"/search"} className={style['nav-link']}>Search Store</Link>
+          <Link to={"/more"} className={style['nav-link']}> More</Link>
+    
         </div>
-
+        <div>
+          <button>Subscribe</button>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
