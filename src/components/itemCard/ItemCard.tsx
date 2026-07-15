fff584d2d8f147f1da5dd0a27349557165afbb3c
SenseiTumelo <tmotjopi@gmail.com>
2026-07-15 13:30:35 +0200

    w

--- src/components/itemCard/ItemCard.tsx
+++ src/components/itemCard/ItemCard.tsx
@@ -1,11 +1,6 @@
 import { Text } from "../Text/Text"
 import style from '../itemCard/ItemCard.module.css'
 
-//card carousel imports n.b: ive installed react multi carousel library
-//import Carousel from "react-multi-carousel"
-//import "react-multi-carousel/lib/styles.css"
-
-
 type ItemCardProps = {
     name: string,
     price: string,
@@ -16,23 +11,7 @@ type ItemCardProps = {
 }
 
 export const ItemCard: React.FC<ItemCardProps> = ({name, price, bgColor, imgSRC,btnColor}) => {
-    /*const responsive = {
-        desktop: {
-            breakpoint: {max: 3000, min: 1024},
-            items: 4,
-            slideToSlide: 3
-        },
-        tablet: {
-            breakpoint: {max: 1024, min: 464},
-            items: 3,
-            slideToSlide: 2
-        },
-        mobile: {
-            breakpoint: {max: 464, min: 0},
-            items: 2,
-            slideToSlide: 1
-        }
-    }*/  
+
     return (
 
       <article className={style["item-card"]}>
