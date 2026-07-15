5292caf82776bea390305b32651bc1b7b4015912
SenseiTumelo <tmotjopi@gmail.com>
2026-07-15 12:02:06 +0200

    hero responnsive

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
   return (
     <div className={style.container}>
       <div className={style['hero-details']}>
-        <Text variant='h1'>A New Kind of Soda</Text>
+        <Text variant='h1'>A New Kind <br/>of Soda</Text>
         <Button text="Shop Olipop" style={{height:"55px", width:"250px", fontSize:"28px"}}/>
       </div>
       <div className={style['hero-card']}>
